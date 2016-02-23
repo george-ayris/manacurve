58ba2729a3dbbdae0b517066a9cde640d707e1af
George Ayris <george.ayris@huddle.com>
2016-02-23 23:16:43 +0000

    Added local caching of api results

--- js-app/js/utils/api.js
+++ js-app/js/utils/api.js
@@ -15,21 +15,51 @@ function numberOfLandsToDeckId(numberOfLandsByColour) {
   return Constants.Colours.reduce(createDeckId, "")
 }
 
+var apiCache = {};
+
+function cacheKey(numberOfLandsByColour, keySuffix) {
+  return numberOfLandsToDeckId(numberOfLandsByColour) + keySuffix;
+}
+
+function addToCache(key) {
+  return (data, textStatus, jqXHR) => {
+    apiCache[key] = data;
+  };
+}
+
+function checkCache(key) {
+  return apiCache[key];
+}
+
+function checkCacheAnd(numberOfLandsByColour, keySuffix, f) {
+  var key = cacheKey(numberOfLandsByColour, keySuffix);
+  var cachedValue = checkCache(key);
+  if (cachedValue) {
+    return $.when(cachedValue);
+  } else {
+    return f().done(addToCache(key));
+  }
+}
+
 export default {
   createDeck(numberOfLandsByColour) {
-    return $.ajax({
-      type: "POST",
-      url: "/deck",
-      data: JSON.stringify(Utils.createApiColoursObject(numberOfLandsByColour)),
-      contentType: "application/json; charset=utf-8"
-    });
+    return checkCacheAnd(numberOfLandsByColour, "", () =>
+      $.ajax({
+        type: "POST",
+        url: "/deck",
+        data: JSON.stringify(Utils.createApiColoursObject(numberOfLandsByColour)),
+        contentType: "application/json; charset=utf-8"
+      })
+    );
   },
 
   getAverages(numberOfLandsByColour) {
-    return $.get('/deck/' + numberOfLandsToDeckId(numberOfLandsByColour) + '/averages');
+    return checkCacheAnd(numberOfLandsByColour, ":averages",
+      () => $.get('/deck/' + numberOfLandsToDeckId(numberOfLandsByColour) + '/averages'));
   },
 
   getMostCommonScenarios(numberOfLandsByColour) {
-    return $.get('/deck/' + numberOfLandsToDeckId(numberOfLandsByColour) + '/mostcommon');
+    return checkCacheAnd(numberOfLandsByColour, ":mostcommon",
+      () => $.get('/deck/' + numberOfLandsToDeckId(numberOfLandsByColour) + '/mostcommon'));
   }
 }
